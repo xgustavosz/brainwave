7074fb4c22420702d656cf12c723d2ac5f0d3601
Gustavo Silva <gugassilva2000@gmail.com>
2024-07-18 20:21:19 -0300

    :sparkles: create skeleton section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
+import Section from "./components/Section";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Section />
       </div>
       <ButtonGradient />
     </>
