5b9252e49476cc4db782793e08c4788ca8fc88b5
Gustavo Silva <gugassilva2000@gmail.com>
2024-07-18 21:04:35 -0300

    :sparkles: create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
-import Section from "./components/Section";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <Section />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
